95b37c6595b9005132b831d2828543090cdb3a9c
Sam-Montoya <samuel.montoya1073@gmail.com>
2017-10-27 09:38:36 -0600

    Started basic mastery page.

--- mastery-app/src/App.js
+++ mastery-app/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './Reset.css';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="main_page_container">
+				<header className="main_page_header">
+					<section>Mastery</section>
+          <section>Home</section>
+				</header>
+				<section>
+					<h1>Sam's Mastery Project</h1>
+				</section>
+			</div>
+		);
+	}
 }
 
 export default App;
