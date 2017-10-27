283ef9518686f55cd89af37ac8bc948ef7a3a456
Sam-Montoya <samuel.montoya1073@gmail.com>
2017-10-27 09:46:54 -0600

    Background image changes

--- mastery-app/src/App.js
+++ mastery-app/src/App.js
@@ -5,14 +5,17 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<div className="main_page_container">
+			<div className="main_page">
 				<header className="main_page_header">
 					<section>Mastery</section>
-          <section>Home</section>
+					<section>Home</section>
 				</header>
-				<section>
-					<h1>Sam's Mastery Project</h1>
-				</section>
+
+				<div className="main_page_container">
+					<section className="hero_background_image">
+						<h1>Sam's Mastery Project</h1>
+					</section>
+				</div>
 			</div>
 		);
 	}
