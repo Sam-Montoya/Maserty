d49f201bd531d72ca16fba5004294e9015a349fa
Sam-Montoya <samuel.montoya1073@gmail.com>
2017-10-30 14:41:53 -0600

    Changed the component

--- mastery-app/src/Components/Redux/Nested.js
+++ mastery-app/src/Components/Redux/Nested.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-export default class Nested extends React.Component {
-	render() {
-		return (
-			<div>
-				I am a nested component with props!
-				<h1>{this.props.username}</h1>
-                <h1>{this.props.match.path}</h1>
-			</div>
-		);
-	}
+export default function (props) {
+	return (
+		<div>
+			I am a nested component with props!
+				<h1>{props.username}</h1>
+			<h1>{props.match.path}</h1>
+		</div>
+	)
 }
